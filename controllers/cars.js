496c89c01bcfbcc0cc3882adc7f4647f87b42e02
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-11-02 10:06:51 -0600

    delete route working

--- controllers/cars.js
+++ controllers/cars.js
@@ -3,16 +3,23 @@ const express = require('express');
 const router = express.Router();
 const Cars = require('../models/cars.js')
 
-
+//index route
 router.get('/', (req, res) => {
     Cars.find({}, (err, foundCar) => {
         res.json(foundCar)
     });
 });
 
+//create route
 router.post('/', (req, res) => {
     Cars.create(req.body, (err, createdCar) => {
         res.json(createdCar)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Cars.findByIdAndRemove(req.params.id, (err, deletedCar) => {
+        res.json(deletedCar)
     })
 })
 
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express()
 //controllers
 const carsController = require('./controllers/cars.js')
 
-
 //middleware
 app.use(express.json())
 app.use('/cars', carsController)
